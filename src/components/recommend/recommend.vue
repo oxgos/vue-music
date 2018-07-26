95b42f62a41348dc260c22131d3cf410c45b5af3
guojianwei <guojianwei@wuxicloud.com>
2018-07-26 17:51:04 +0800

    添加图片懒加载，loading组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,8 @@
             <slider>
               <div v-for="(item, index) in recommends" :key="index">
                 <a :href="item.linkUrl">
-                  <img @load="loadImage" :src="item.picUrl" alt="slider">
+                  <!-- 加class needsclick后,fastclick不会阻止默认行为 -->
+                  <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="slider">
                 </a>
             </div>
             </slider>
@@ -16,7 +17,7 @@
             <ul>
               <li @click="selectItem(item)" v-for="(item, index) in discList" :key="index" class="item">
                 <div class="icon">
-                  <img width="60" height="60" :src="item.imgurl">
+                  <img width="60" height="60" v-lazy="item.imgurl">
                 </div>
                 <div class="text">
                   <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,6 +27,9 @@
             </ul>
           </div>
         </div>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
+        </div>
       </scroll>
     </div>
 </template>
@@ -33,6 +37,7 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
+  import Loading from 'base/loading/loading'
   import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
 
@@ -80,7 +85,8 @@
       },
       components: {
         Scroll,
-        Slider
+        Slider,
+        Loading
       }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 // 解决移动端浏览器点击300毫秒的延迟
 fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 Vue.config.productionTip = false
 
